c46e989416ec01adbfacf57bb341d5e0b757841a
brosling <broslin1@umbc.edu>
2025-10-27 23:42:04 -0400

    Fixed bugs with current_visas to also check to make sure visa wasn't expired and make a new column of the clean version of expiration date. Added new function visas_to_renew that returns list of dictionaries of visas to renew. Updated main.py to write visas_to_renew result to txt file

--- back/excel_parsing.py
+++ back/excel_parsing.py
@@ -3,6 +3,8 @@ import pandas as pd
 #returns excel sheet parsed by pandas
 def get_excel():
     excel = pd.read_excel("Case tracking for CS class.xlsx")
+    mask = excel["Start date"].map(type) == str
+    excel.loc[mask, "Start date"] = (excel.loc[mask, "Start date"].str.extract(r'(\d{1,2}[/]\d{1,2}[/]\d{2,4})')[0])
     excel["Start date"] = pd.to_datetime(excel["Start date"], errors='coerce')
     return excel
 
@@ -12,6 +14,41 @@ def current_visas(excel):
     excel_sorted = excel.sort_values(by="Start date", ascending=False)
     today = pd.Timestamp.today().normalize()
     excel_sorted = excel_sorted[excel_sorted["Start date"] < today]
-    #excel_sorted = excel_sorted[excel_sorted["Expiration Date"] > today]
     excel_sorted = excel_sorted.drop_duplicates(subset=["Last name", "First Name"], keep = 'first')
+    excel_sorted["clean exp date"] = excel_sorted["Expiration Date"]
+    mask = excel_sorted["clean exp date"].map(type) == str
+    excel_sorted.loc[mask, "clean exp date"] = (excel_sorted.loc[mask, "clean exp date"].str.extract(r'(\d{1,2}[/]\d{1,2}[/]\d{2,4})')[0])
+    excel_sorted["clean exp date"] = pd.to_datetime(excel_sorted["clean exp date"], errors='coerce')
+    excel_sorted = excel_sorted[excel_sorted["clean exp date"] >= today]
     return excel_sorted
+
+
+def visas_to_renew(excel):
+    #excel: dataframe of sorted excel sheet containing only current visas
+    #returns list of dictionaries containing info on all people whos visas are up for renewal
+    today = pd.Timestamp.today().normalize()
+    renew_list = []
+    for _, row in excel.iterrows():
+        if "done" in str(row["Expiration Date"]):
+            continue
+
+        case_type = str(row["Case type"])
+        if ("H-1B" in case_type) or ("J-1" in case_type):
+            if((row["clean exp date"] - today).days <= 180):
+                renew_list.append({
+                        "Last name": row["Last name"],
+                        "First name": row["First Name"],
+                        "Case type": row["Case type"],
+                        "Expiration date": str((row["clean exp date"].date())),
+                    })
+            continue
+        if ("F-1" in case_type):
+            if((row["clean exp date"] - today).days <= 365):
+                renew_list.append({
+                        "Last name": row["Last name"],
+                        "First name": row["First Name"],
+                        "Case type": row["Case type"],
+                        "Expiration date": str((row["clean exp date"].date())),
+                    })
+
+    return renew_list
