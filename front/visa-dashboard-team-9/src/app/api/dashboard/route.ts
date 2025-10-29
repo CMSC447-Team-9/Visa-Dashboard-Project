24636d5e183509c71e71c6ae5344fda6cad02994
Chujia Guo <herobrinevinyl@gmail.com>
2025-10-28 22:50:22 -0400

    Adding test data API

--- front/visa-dashboard-team-9/src/app/api/dashboard/route.ts
+++ front/visa-dashboard-team-9/src/app/api/dashboard/route.ts
@@ -1,24 +1,43 @@
 import { NextResponse } from "next/server"
 import { EmployeeRecord } from "@/app/types/EmployeeRecord"
 import { DashboardData } from "@/app/types/DashboardData"
+import { testData } from "@/app/data/testEntries"
 
 export async function GET() {
-    const dummyData: EmployeeRecord[] = []
+    const dummyData: EmployeeRecord[] = getUniqueEmployeesByMostRecent(testData)
     const visaTypes = {
-        h1b: 1,
-        j1: 2,
-        f1: 3,
-        residency: 4
+        h1b: 999,
+        j1: 999,
+        f1: 999,
+        residency: 999
     }
     const data: DashboardData = {
         status: "success",
         timestamp: Date.now().toLocaleString('en-US'),
-        active: 2,
-        expiring: 3,
+        active: -999,
+        expiring: -999,
         visaTypes: visaTypes,
         entry_count: dummyData.length,
         entries: dummyData,
     }
 
     return NextResponse.json(data, { status: 200 })
+}
+
+function getUniqueEmployeesByMostRecent(records: EmployeeRecord[]): EmployeeRecord[] {
+    const map = new Map<string, EmployeeRecord>();
+
+    for (const rec of records) {
+        const key = rec.employeeUmbcEmail;
+        const existing = map.get(key);
+
+        const recDate = new Date(rec.startDate).getTime();
+        const existingDate = existing ? new Date(existing.startDate).getTime() : 0;
+
+        if (!existing || recDate > existingDate) {
+            map.set(key, rec);
+        }
+    }
+
+    return Array.from(map.values());
 }
