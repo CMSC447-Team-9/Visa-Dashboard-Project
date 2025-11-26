abab4b70e511a67cea30c797f8073640b2081572
ColinStoke <colinstokes.business@gmail.com>
2025-11-26 17:54:14 -0500

    Baby work so far, will finish wireframe by tonight!!!

--- front/src/app/dashboard/page.tsx
+++ front/src/app/dashboard/page.tsx
@@ -1,7 +1,25 @@
 export default function Dashboard() {
+
+    /*
+    -Turn current dashboard into reporting page based on feedback from Diane
+    -Move visa type numbers from reporting page to dashboard and make them more compact (I have ideas for that)
+    -Add new ui element on the dashboard for cases per: academic year, calendar year, and reporting year per case types
+    -Add result number to reporting page
+    -Add export to csv option on reporting page
+    -Add reload button on sidebar to manually reload data
+    */
+
     return (
-        <div>
-            Dashboard Page
+    
+        <div className="flex w-full h-full justify-evenly place-content-center">
+            <div className="rounded-xl border bg-[#B6B7B9] flex flex-col p-4 w-5/10 h-4/5">
+                Upcoming Cases 
+            </div>
+
+            <div className="rounded-xl border bg-[#B6B7B9] flex flex-col p-4 w-3/10 h-4/5">
+                Case Numbers
+            </div>
         </div>
+        
     )
 }
