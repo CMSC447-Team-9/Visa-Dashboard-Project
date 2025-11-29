bc6d18381623afe941402bc887ef4974799d4a81
Chujia Guo <herobrinevinyl@gmail.com>
2025-11-28 20:58:53 -0500

    Skeleton for Individual Page

---
+++ front/src/app/dashboard/[email]/page.tsx
@@ -0,0 +1,15 @@
+interface PageProps {
+    params: {
+        email: string
+    }
+}
+
+export default async function DashboardByEmail({ params }: PageProps) {
+    const { email } = await params
+
+    return (
+        <div>
+            <h1>Dashboard for {email}</h1>
+        </div>
+    );
+}
